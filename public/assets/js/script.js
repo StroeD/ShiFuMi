34a83ceb5cae33025274bb8020d0e8397eaf13d4
Dimitri Stroe <stroe.dimitri@gmail.com>
2022-05-13 15:36:48 +0200

    gdfjhdgf

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,55 +7,55 @@ let results = [end,lastCase,userInput];
 let i = 0; // player Win Counter
 let j = 0; // computer Win Counter
 
-document.getElementById('rock').addEventListener('click',function(){
-    results = AI(results[0],results[1],0);
-    if (results[0] != 0){
-        i++
-    }
-    if (results[0] != 1){
-        j++
-    }
-    if (i == 10 && j ==10){
-        alert('Tie!')
-    }else if(i == 10){
-        alert('Win!')
-    }else if(j == 10){
-        alert('| ||\n|i | _')
-    }
-    alert(results[0]+ ' ' + results[1] + ' ' + results[2] + ' Player: ' + i + 'Computer: ' + j);
-})
-document.getElementById('paper').addEventListener('click',function(){
-    results = AI(results[0],results[1],1);
-    if (results[0] != 0){
-        i++
-    }
-    if (results[0] != 1){
-        j++
-    }
-    if (i == 10 && j ==10){
-        alert('Tie!')
-    }else if(i == 10){
-        alert('Win!')
-    }else if(j == 10){
-        alert('| ||\n|i | _')
-    }
-    alert(results[0]+ ' ' + results[1] + ' ' + results[2] + ' Player: ' + i + 'Computer: ' + j);
-})
+// document.getElementById('rock').addEventListener('click',function(){
+//     results = AI(results[0],results[1],0);
+//     if (results[0] != 0){
+//         i++
+//     }
+//     if (results[0] != 1){
+//         j++
+//     }
+//     if (i == 10 && j ==10){
+//         alert('Tie!')
+//     }else if(i == 10){
+//         alert('Win!')
+//     }else if(j == 10){
+//         alert('| ||\n|i | _')
+//     }
+//     alert(results[0]+ ' ' + results[1] + ' ' + results[2] + ' Player: ' + i + 'Computer: ' + j);
+// })
+// document.getElementById('paper').addEventListener('click',function(){
+//     results = AI(results[0],results[1],1);
+//     if (results[0] != 0){
+//         i++
+//     }
+//     if (results[0] != 1){
+//         j++
+//     }
+//     if (i == 10 && j ==10){
+//         alert('Tie!')
+//     }else if(i == 10){
+//         alert('Win!')
+//     }else if(j == 10){
+//         alert('| ||\n|i | _')
+//     }
+//     alert(results[0]+ ' ' + results[1] + ' ' + results[2] + ' Player: ' + i + 'Computer: ' + j);
+// })
 
-document.getElementById('scissors').addEventListener('click',function(){
-    results = AI(results[0],results[1],2);
-    if (results[0] != 0){
-        i++
-    }
-    if (results[0] != 1){
-        j++
-    }
-    if (i == 10 && j ==10){
-        alert('Tie!')
-    }else if(i == 10){
-        alert('Win!')
-    }else if(j == 10){
-        alert('| ||\n|i | _')
-    }
-    alert(results[0]+ ' ' + results[1] + ' ' + results[2] + ' Player: ' + i + 'Computer: ' + j);
-})
+// document.getElementById('scissors').addEventListener('click',function(){
+//     results = AI(results[0],results[1],2);
+//     if (results[0] != 0){
+//         i++
+//     }
+//     if (results[0] != 1){
+//         j++
+//     }
+//     if (i == 10 && j ==10){
+//         alert('Tie!')
+//     }else if(i == 10){
+//         alert('Win!')
+//     }else if(j == 10){
+//         alert('| ||\n|i | _')
+//     }
+//     alert(results[0]+ ' ' + results[1] + ' ' + results[2] + ' Player: ' + i + 'Computer: ' + j);
+// })
